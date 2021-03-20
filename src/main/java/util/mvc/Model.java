da6397ae299dff980a1176ed8cc59429027cfb05
Stefan Aladzic <69090391+saladzic@users.noreply.github.com>
2021-03-20 19:42:15 +0100

    + Model for better control over database e.g. table prefix

--- src/main/java/util/mvc/Model.java
+++ src/main/java/util/mvc/Model.java
@@ -1,6 +1,19 @@
 package util.mvc;
 
+import util.driver.Db;
 import util.main.Kernel;
 
+import java.sql.Connection;
+
 public abstract class Model extends Kernel {
+    private Connection connection;
+
+    public Model() {
+        Db db = new Db();
+        connection = db.getDb();
+    }
+
+    public Connection getDb() {
+        return connection;
+    }
 }
