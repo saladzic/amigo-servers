e7bdde2cee224177ee6b8824af3a499318ec5f7f
Stefan Aladzic <69090391+saladzic@users.noreply.github.com>
2021-03-20 12:52:30 +0100

    + log and text block structure

--- database/db.sql
+++ database/db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `amigo_user` (
+CREATE TABLE IF NOT EXISTS `amigo_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL DEFAULT '',
   `password` varchar(255) DEFAULT NULL,
@@ -6,4 +6,31 @@ CREATE TABLE `amigo_user` (
   `email` varchar(150) DEFAULT NULL,
   `ip_address` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `amigo_log_system` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `type` varchar(10) NOT NULL,
+ `message` varchar(150) NOT NULL,
+ `created_at` int(11) NOT NULL,
+ `active` tinyint(1) NOT NULL DEFAULT '1',
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `amigo_log_user` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `user_id` int(11) NOT NULL,
+ `type` varchar(10) NOT NULL,
+ `heading_block` varchar(150) NOT NULL,
+ `message_block` varchar(150) NOT NULL,
+ `active` tinyint(1) NOT NULL DEFAULT '1',
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `amigo_text_block` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(150) NOT NULL,
+ `lang` varchar(5) NOT NULL,
+ `text` varchar(250) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
