6fe6ce8fa7b2404c270581f320d1ea1fe13b9272
Stefan Aladzic <69090391+saladzic@users.noreply.github.com>
2021-03-20 18:56:34 +0100

    + text block translator

---
+++ src/main/java/cms/TextBlock.java
@@ -0,0 +1,23 @@
+package cms;
+
+import util.mvc.Model;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TextBlock extends Model {
+    public String translate(String lang, String block) {
+        try {
+            PreparedStatement stmt = getDb().prepareStatement("SELECT text " +
+                    "FROM amigo_text_block WHERE lang=? AND name=?");
+            ResultSet res = stmt.executeQuery();
+            if (res.next()) {
+                return res.getString("text");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return block;
+    }
+}
