71d35b6ad151e17afc2fe962fd1a7aac039f57f9
Stefan Aladzic <69090391+saladzic@users.noreply.github.com>
2021-03-20 19:39:50 +0100

    + Log system with unit tests

---
+++ src/main/java/log/AbstractLog.java
@@ -0,0 +1,51 @@
+package log;
+
+import util.mvc.Model;
+
+public abstract class AbstractLog extends Model {
+    private int id;
+    private String type;
+    private String heading;
+    private String message;
+    private boolean active;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
