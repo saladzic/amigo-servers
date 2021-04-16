92eedf4cbfd9e562fbc99e1f7c80d3a912dacd9e
Stefan Aladzic <69090391+saladzic@users.noreply.github.com>
2021-04-16 05:02:45 +0200

    Remove genRandString() since it is a duplicate method

--- src/main/java/com/amigoservers/backend/util/main/Kernel.java
+++ src/main/java/com/amigoservers/backend/util/main/Kernel.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.Reader;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Random;
 
 public class Kernel {
     public static void main(String[] args) {
@@ -38,21 +37,8 @@ public class Kernel {
             //Running the script
             sr.runScript(reader);
             sr.runScript(reader2);
-        } catch (SQLException exception) {
+        } catch (SQLException | IOException exception) {
             exception.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
-
-    public String genRandString(int length) {
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        Random random = new Random();
-        return random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(length)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
     }
 }
